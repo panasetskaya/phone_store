0118e8a903195b7fee9cb9c6f4d74aba0f3c60a3
Maria Panasetskaia <mariapolikutina@gmail.com>
2022-11-05 17:35:58 +0800

    Initial commit

---
+++ app/src/main/java/com/panasetskaia/phonestore/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.panasetskaia.phonestore.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.panasetskaia.phonestore.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
