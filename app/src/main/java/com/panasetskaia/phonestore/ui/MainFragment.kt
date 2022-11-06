45e9e8c7916b8b79b70251034b72b659490fc6a6
Maria Panasetskaia <mariapolikutina@gmail.com>
2022-11-06 23:01:37 +0800

    spinners

--- app/src/main/java/com/panasetskaia/phonestore/ui/MainFragment.kt
+++ app/src/main/java/com/panasetskaia/phonestore/ui/MainFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Spinner
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.panasetskaia.phonestore.R
 
@@ -14,17 +16,18 @@ class MainFragment : Fragment() {
 
     lateinit var closeDialog: ImageView
     lateinit var applyFilters: Button
+    lateinit var bottomSheetDialog: BottomSheetDialog
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        bottomSheetDialog = BottomSheetDialog(requireContext())
         setListeners()
     }
 
@@ -36,9 +39,10 @@ class MainFragment : Fragment() {
     }
 
     private fun showBottomSheetDialog() {
-
-        val bottomSheetDialog = BottomSheetDialog(requireContext())
         bottomSheetDialog.setContentView(R.layout.bottom_sheet_dialog_layout)
+        setSpinner(bottomSheetDialog.findViewById(R.id.spinner_brand)!!, R.array.brands_array)
+        setSpinner(bottomSheetDialog.findViewById(R.id.spinner_price)!!, R.array.price_array)
+        setSpinner(bottomSheetDialog.findViewById(R.id.spinner_size)!!, R.array.size_array)
         closeDialog = bottomSheetDialog.findViewById(R.id.close_dialog_button)!!
         closeDialog.setOnClickListener {
             bottomSheetDialog.dismiss()
@@ -47,9 +51,17 @@ class MainFragment : Fragment() {
         applyFilters.setOnClickListener {
             bottomSheetDialog.dismiss()
         }
-
         bottomSheetDialog.show()
+    }
 
-
+    private fun setSpinner(spinner: Spinner, array: Int) {
+        ArrayAdapter.createFromResource(
+            this@MainFragment.requireContext(),
+            array,
+            R.layout.spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(R.layout.spinner_item)
+            spinner.adapter = adapter
+        }
     }
 }
